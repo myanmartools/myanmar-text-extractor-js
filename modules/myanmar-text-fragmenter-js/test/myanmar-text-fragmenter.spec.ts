6e7035efc5e03a0392247bfdb86ef6b410f828f7
Moe Myat Zaw <mmzliveid@gmail.com>
2019-12-24 21:31:04 +0630

    Update tests

--- modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter.spec.ts
+++ modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter.spec.ts
@@ -11,10 +11,64 @@ describe('MyanmarTextFragmenter#getNextFragment', () => {
         fragmenter = new MyanmarTextFragmenter();
     });
 
-    it(String.raw`should return fragement '\u1024' when input '\u1024'`, () => {
+    it(String.raw`should return fragement '\u1024' when input '\u1024' (standalone letter)`, () => {
         const fragment = fragmenter.getNextFragment('\u1024') as TextFragment;
 
         expect(fragment.matchedString).toBe('\u1024',
+            `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
+        expect(fragment.standaloneLetter).toBeTruthy();
+        expect(fragment.spaceIncluded).toBeFalsy();
+    });
+
+    it(String.raw`should return fragement '\u102A' when input '\u102A' (standalone letter)`, () => {
+        const fragment = fragmenter.getNextFragment('\u102A') as TextFragment;
+
+        expect(fragment.matchedString).toBe('\u102A',
+            `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
+        expect(fragment.standaloneLetter).toBeTruthy();
+        expect(fragment.spaceIncluded).toBeFalsy();
+    });
+
+    it(String.raw`should return fragement '\u104A' when input '\u104A' (standalone letter)`, () => {
+        const fragment = fragmenter.getNextFragment('\u104A') as TextFragment;
+
+        expect(fragment.matchedString).toBe('\u104A',
+            `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
+        expect(fragment.standaloneLetter).toBeTruthy();
+        expect(fragment.spaceIncluded).toBeFalsy();
+    });
+
+    it(String.raw`should return fragement '\u104B' when input '\u104B' (standalone letter)`, () => {
+        const fragment = fragmenter.getNextFragment('\u104B') as TextFragment;
+
+        expect(fragment.matchedString).toBe('\u104B',
+            `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
+        expect(fragment.standaloneLetter).toBeTruthy();
+        expect(fragment.spaceIncluded).toBeFalsy();
+    });
+
+    it(String.raw`should return fragement '\u104C' when input '\u104C' (standalone letter)`, () => {
+        const fragment = fragmenter.getNextFragment('\u104C') as TextFragment;
+
+        expect(fragment.matchedString).toBe('\u104C',
+            `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
+        expect(fragment.standaloneLetter).toBeTruthy();
+        expect(fragment.spaceIncluded).toBeFalsy();
+    });
+
+    it(String.raw`should return fragement '\u104D' when input '\u104D' (standalone letter)`, () => {
+        const fragment = fragmenter.getNextFragment('\u104D') as TextFragment;
+
+        expect(fragment.matchedString).toBe('\u104D',
+            `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
+        expect(fragment.standaloneLetter).toBeTruthy();
+        expect(fragment.spaceIncluded).toBeFalsy();
+    });
+
+    it(String.raw`should return fragement '\u104F' when input '\u104F' (standalone letter)`, () => {
+        const fragment = fragmenter.getNextFragment('\u104F') as TextFragment;
+
+        expect(fragment.matchedString).toBe('\u104F',
             `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
         expect(fragment.standaloneLetter).toBeTruthy();
         expect(fragment.spaceIncluded).toBeFalsy();
