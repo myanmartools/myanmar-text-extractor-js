8e21f64fbbc551a54c3a2ce771078515a773b43d
Moe Myat Zaw <mmzliveid@gmail.com>
2019-12-25 10:22:48 +0630

    Update tests

--- modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter.spec.ts
+++ modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter.spec.ts
@@ -11,7 +11,8 @@ describe('MyanmarTextFragmenter#getNextFragment', () => {
         fragmenter = new MyanmarTextFragmenter();
     });
 
-    it(String.raw`should return fragement '\u1024' when input '\u1024' (standalone letter)`, () => {
+    // ဤ -> ဤ
+    it(String.raw`should return fragement '\u1024' when input '\u1024'`, () => {
         const fragment = fragmenter.getNextFragment('\u1024') as TextFragment;
 
         expect(fragment.matchedString).toBe('\u1024',
@@ -20,7 +21,8 @@ describe('MyanmarTextFragmenter#getNextFragment', () => {
         expect(fragment.error).toBeFalsy();
     });
 
-    it(String.raw`should return fragement '\u102A' when input '\u102A' (standalone letter)`, () => {
+    // ဪ -> ဪ
+    it(String.raw`should return fragement '\u102A' when input '\u102A'`, () => {
         const fragment = fragmenter.getNextFragment('\u102A') as TextFragment;
 
         expect(fragment.matchedString).toBe('\u102A',
@@ -29,7 +31,8 @@ describe('MyanmarTextFragmenter#getNextFragment', () => {
         expect(fragment.error).toBeFalsy();
     });
 
-    it(String.raw`should return fragement '\u104A' when input '\u104A' (standalone letter)`, () => {
+    // ၊ -> ၊
+    it(String.raw`should return fragement '\u104A' when input '\u104A'`, () => {
         const fragment = fragmenter.getNextFragment('\u104A') as TextFragment;
 
         expect(fragment.matchedString).toBe('\u104A',
@@ -38,7 +41,8 @@ describe('MyanmarTextFragmenter#getNextFragment', () => {
         expect(fragment.error).toBeFalsy();
     });
 
-    it(String.raw`should return fragement '\u104B' when input '\u104B' (standalone letter)`, () => {
+    // ။ -> ။
+    it(String.raw`should return fragement '\u104B' when input '\u104B'`, () => {
         const fragment = fragmenter.getNextFragment('\u104B') as TextFragment;
 
         expect(fragment.matchedString).toBe('\u104B',
@@ -47,7 +51,8 @@ describe('MyanmarTextFragmenter#getNextFragment', () => {
         expect(fragment.error).toBeFalsy();
     });
 
-    it(String.raw`should return fragement '\u104C' when input '\u104C' (standalone letter)`, () => {
+    // ၌ -> ၌
+    it(String.raw`should return fragement '\u104C' when input '\u104C'`, () => {
         const fragment = fragmenter.getNextFragment('\u104C') as TextFragment;
 
         expect(fragment.matchedString).toBe('\u104C',
@@ -56,7 +61,8 @@ describe('MyanmarTextFragmenter#getNextFragment', () => {
         expect(fragment.error).toBeFalsy();
     });
 
-    it(String.raw`should return fragement '\u104D' when input '\u104D' (standalone letter)`, () => {
+    // ၍ -> ၍
+    it(String.raw`should return fragement '\u104D' when input '\u104D'`, () => {
         const fragment = fragmenter.getNextFragment('\u104D') as TextFragment;
 
         expect(fragment.matchedString).toBe('\u104D',
@@ -65,10 +71,21 @@ describe('MyanmarTextFragmenter#getNextFragment', () => {
         expect(fragment.error).toBeFalsy();
     });
 
-    it(String.raw`should return fragement '\u104F' when input '\u104F' (standalone letter)`, () => {
+    // ၏ -> ၏
+    it(String.raw`should return fragement '\u104F' when input '\u104F'`, () => {
         const fragment = fragmenter.getNextFragment('\u104F') as TextFragment;
 
         expect(fragment.matchedString).toBe('\u104F',
+            `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
+        expect(fragment.uncombinableLetter).toBeTruthy();
+        expect(fragment.error).toBeFalsy();
+    });
+
+    // ဤက -> ဤ
+    it(String.raw`should return fragement '\u1024' when input '\u1024\u1000'`, () => {
+        const fragment = fragmenter.getNextFragment('\u1024\u1000') as TextFragment;
+
+        expect(fragment.matchedString).toBe('\u1024',
             `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
         expect(fragment.uncombinableLetter).toBeTruthy();
         expect(fragment.error).toBeFalsy();
