f8dda8f476ebb2a49a7f07aa7690cce658a861f5
Moe Myat Zaw <mmzliveid@gmail.com>
2019-12-28 00:03:19 +0630

    Update tests

--- modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter-digit.spec.ts
+++ modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter-digit.spec.ts
@@ -11,11 +11,30 @@ describe('MyanmarTextFragmenter#getNextFragment#digit', () => {
         fragmenter = new MyanmarTextFragmenter();
     });
 
-    it(String.raw`'\u1040' -> '\u1040'`, () => {
+    it(String.raw`should return numberFragment when input '\u1040'`, () => {
         const fragment = fragmenter.getNextFragment('\u1040') as TextFragment;
 
         expect(fragment.matchedStr).toBe('\u1040',
             `\n\nActual matchedStr: ${formatCodePoints(fragment.matchedStr)}`);
+        expect(fragment.numberFragment).toBeTruthy();
+        expect(fragment.error).toBeFalsy();
+    });
+
+    it(String.raw`should return numberFragment when input '\u1049'`, () => {
+        const fragment = fragmenter.getNextFragment('\u1049') as TextFragment;
+
+        expect(fragment.matchedStr).toBe('\u1049',
+            `\n\nActual matchedStr: ${formatCodePoints(fragment.matchedStr)}`);
+        expect(fragment.numberFragment).toBeTruthy();
+        expect(fragment.error).toBeFalsy();
+    });
+
+    it(String.raw`should return numberFragment when box order list input '(\u1041)'`, () => {
+        const fragment = fragmenter.getNextFragment('(\u1041)') as TextFragment;
+
+        expect(fragment.matchedStr).toBe('(\u1041)',
+            `\n\nActual matchedStr: ${formatCodePoints(fragment.matchedStr)}`);
+        expect(fragment.numberFragment).toBeTruthy();
         expect(fragment.error).toBeFalsy();
     });
 });
