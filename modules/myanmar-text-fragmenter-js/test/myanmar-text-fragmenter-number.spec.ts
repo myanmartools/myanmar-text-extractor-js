ce7a4a0f853ff5d7e98fc262b7aabe2c7eef21f7
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-05 20:16:06 +0630

    Update tests

--- modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter-number.spec.ts
+++ modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter-number.spec.ts
@@ -148,4 +148,16 @@ describe('MyanmarTextFragmenter#getNextFragment#number', () => {
         expect(fragment.digitStr).toBe('၁');
         expect(fragment.numberOrderList).toBeTruthy();
     });
+
+    it(String.raw`should return order list number fragment when input '(၉၀)'`, () => {
+        const input = '(၉၀)';
+        const fragment = fragmenter.getNextFragment(input) as TextFragment;
+
+        expect(fragment.matchedStr).toBe(input,
+            `\n\nActual matchedStr: ${formatCodePoints(fragment.matchedStr)}`);
+        expect(fragment.normalizedStr).toBe(input);
+        expect(fragment.fragmentType).toEqual(FragmentType.Number);
+        expect(fragment.digitStr).toBe('၉၀');
+        expect(fragment.numberOrderList).toBeTruthy();
+    });
 });
