77cebfb98b25261debb0abd107db4fe84a62ca40
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-11 11:51:43 +0630

    Update tests

--- modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter-number.spec.ts
+++ modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter-number.spec.ts
@@ -163,6 +163,20 @@ describe('MyanmarTextFragmenter#getNextFragment#number', () => {
         });
     });
 
+    it(String.raw`should return number fragment when input with separator and decimal dot '၃.၁၄၁၅၉၂၆၅၃၅'`, () => {
+        const input = '၃.၁၄၁၅၉၂၆၅၃၅';
+        const fragment = fragmenter.getNextFragment(input) as TextFragment;
+
+        expect(fragment.matchedStr).toBe(input, `\n\nActual matchedStr: ${formatCodePoints(fragment.matchedStr)}`);
+        expect(fragment).toEqual({
+            matchedStr: input,
+            normalizedStr: input,
+            fragmentType: FragmentType.Number,
+            digitStr: '၃.၁၄၁၅၉၂၆၅၃၅',
+            digitSeparatorIncluded: true
+        });
+    });
+
     it(String.raw`should return 'အင်္ဂါ' number fragment when input 'င်္၁ါ'`, () => {
         const input = 'င်္၁ါ';
         const fragment = fragmenter.getNextFragment(input) as TextFragment;
@@ -492,5 +506,4 @@ describe('MyanmarTextFragmenter#getNextFragment#number', () => {
             invalidU104EInsteadOfU1044: true
         });
     });
-
 });
