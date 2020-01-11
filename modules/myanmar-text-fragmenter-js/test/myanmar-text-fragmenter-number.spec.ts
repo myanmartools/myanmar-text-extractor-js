5d18a413d807171d9f47f7394877669053180add
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-11 21:44:03 +0630

    Update tests

--- modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter-number.spec.ts
+++ modules/myanmar-text-fragmenter-js/test/myanmar-text-fragmenter-number.spec.ts
@@ -120,6 +120,20 @@ describe('MyanmarTextFragmenter#getNextFragment#number', () => {
         });
     });
 
+    it(String.raw`should return number fragment when input with separator and decimal dot '၁_၂၃၄_၅၆၇.၈၉'`, () => {
+        const input = '၁_၂၃၄_၅၆၇.၈၉';
+        const fragment = fragmenter.getNextFragment(input) as TextFragment;
+
+        expect(fragment.matchedStr).toBe(input, `\n\nActual matchedStr: ${formatCodePoints(fragment.matchedStr)}`);
+        expect(fragment).toEqual({
+            matchedStr: input,
+            normalizedStr: '၁_၂၃၄_၅၆၇.၈၉',
+            fragmentType: FragmentType.Number,
+            digitStr: '၁၂၃၄၅၆၇.၈၉',
+            digitSeparatorIncluded: true
+        });
+    });
+
     it(String.raw`should return number fragment when input with separator and decimal dot '၁⎖၂၃၄⎖၅၆၇.၈၉'`, () => {
         const input = '၁⎖၂၃၄⎖၅၆၇.၈၉';
         const fragment = fragmenter.getNextFragment(input) as TextFragment;
