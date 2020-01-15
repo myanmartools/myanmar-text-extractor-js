7dd97c9ba2f495f104e347712116997ee3abd8d0
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-15 23:26:29 +0630

    Update tests

--- test/specs/myanmar-text-fragmenter-temp.spec.ts
+++ test/specs/myanmar-text-fragmenter-temp.spec.ts
@@ -1,29 +1,23 @@
-// import { FragmentType } from '../src/fragment-type';
-// import { MyanmarTextFragmenter } from '../src/myanmar-text-fragmenter';
-// import { TextFragment } from '../src/text-fragment';
+import { FragmentType } from '../../src/fragment-type';
+import { MyanmarTextFragmenter } from '../../src/myanmar-text-fragmenter';
+import { TextFragment } from '../../src/text-fragment';
 
-// import { formatCodePoints } from './shared/format-code-points.spec';
+describe('MyanmarTextFragmenter#temp', () => {
+    let fragmenter: MyanmarTextFragmenter;
 
-// describe('MyanmarTextFragmenter#temp', () => {
-//     let fragmenter: MyanmarTextFragmenter;
+    beforeEach(() => {
+        fragmenter = new MyanmarTextFragmenter();
+    });
 
-//     beforeEach(() => {
-//         fragmenter = new MyanmarTextFragmenter();
-//     });
+    it(String.raw`should return fragment'`, () => {
+        const input = '(၁)';
+        const fragment = fragmenter.getNextFragment(input) as TextFragment;
 
-//     it(String.raw`should return 'အင်္ဂါ' number fragment when input '၂င်္၀ါ'`, () => {
-//         const input = '၂င်္၀ါ';
-//         const fragment = fragmenter.getNextFragment(input) as TextFragment;
-
-//         expect(fragment.matchedStr).toBe(input,
-//             `\n\nActual matchedStr: ${formatCodePoints(fragment.matchedStr)}`);
-//         expect(fragment).toEqual({
-//             matchedStr: input,
-//             normalizedStr: input,
-//             fragmentType: FragmentType.Number,
-//             digitStr: '၂၀',
-//             ancient: true,
-//             measureWords: ['အင်္ဂါ']
-//         });
-//     });
-// });
+        expect(fragment).toEqual({
+            matchedStr: input,
+            normalizedStr: input,
+            fragmentType: FragmentType.Number,
+            digitStr: '၁'
+        });
+    });
+});
