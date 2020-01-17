a05f8a52cd4efa9817984128aa0e7fdededa88cd
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-18 00:59:34 +0630

    Update tests

--- test/specs/myanmar-text-fragmenter-number-date.spec.ts
+++ test/specs/myanmar-text-fragmenter-number-date.spec.ts
@@ -9,8 +9,9 @@ describe('MyanmarTextFragmenter#getNextFragment#number#date', () => {
         fragmenter = new MyanmarTextFragmenter();
     });
 
-    it(String.raw`should return date number fragment when input '၁၇၊၁၊၂၀၂၀'`, () => {
-        const input = '၁၇၊၁၊၂၀၂၀';
+    // dd၊MM၊yyyy
+    it(String.raw`should return date fragment when input '၀၁၊၀၁၊၂၀၂၀'`, () => {
+        const input = '၀၁၊၀၁၊၂၀၂၀';
         const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
         const expactedFragment: TextFragment = {
             matchedStr: input,
@@ -22,7 +23,29 @@ describe('MyanmarTextFragmenter#getNextFragment#number#date', () => {
         expect(actualFragment).toEqual(expactedFragment);
     });
 
-    it(String.raw`should return date number fragment when input '၁၇ ၊ ၁ ၊ ၂၀၂၀'`, () => {
+    // dd၊MM၊yyyy
+    it(String.raw`should return date fragment when input '၃၁၊၁၂၊၂၀၂၀'`, () => {
+        const input = '၃၁၊၁၂၊၂၀၂၀';
+        const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+        const expactedFragment: TextFragment = {
+            matchedStr: input,
+            normalizedStr: input,
+            fragmentType: FragmentType.PossibleDate,
+            separatorIncluded: true
+        };
+
+        expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    // NOT
+    it(String.raw`should NOT return date fragment when input '၃၂၊၁၂၊၂၀၂၀'`, () => {
+        const input = '၃၂၊၁၂၊၂၀၂၀';
+        const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+
+        expect(actualFragment.fragmentType !== FragmentType.PossibleDate).toBeFalsy();
+    });
+
+    it(String.raw`should return date fragment when input '၁၇ ၊ ၁ ၊ ၂၀၂၀'`, () => {
         const input = '၁၇ ၊ ၁ ၊ ၂၀၂၀';
         const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
         const expactedFragment: TextFragment = {
