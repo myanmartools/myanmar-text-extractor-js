ae70959fd62a43a6e3126fed1acae9347d77000b
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-20 11:03:17 +0630

    Update NormalizationReason model

--- src/normalization-reason.ts
+++ src/normalization-reason.ts
@@ -1,6 +1,7 @@
 export interface NormalizationReason {
     normalizePlusSign?: boolean;
     normalizeSpace?: boolean;
+    removeSpace?: boolean;
     removeInvisibleSpace?: boolean;
     changeU101DToU1040?: boolean;
     changeU104EToU1044?: boolean;
