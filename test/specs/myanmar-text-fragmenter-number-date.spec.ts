dd338f9c4876eb59a06e49e34634a5aa8ac090bf
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-20 21:10:46 +0630

    Update tests

--- test/specs/myanmar-text-fragmenter-number-date.spec.ts
+++ test/specs/myanmar-text-fragmenter-number-date.spec.ts
@@ -421,5 +421,12 @@ describe('MyanmarTextFragmenter#getNextFragment#number#date', () => {
 
             expect(actualFragment.fragmentType !== FragmentType.PossibleDate).toBeTruthy();
         });
+
+        it(String.raw`should NOT return date fragment when input '၀၁-၀၁-၂၀၂၀@'`, () => {
+            const input = '၀၁-၀၁-၂၀၂၀@';
+            const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+
+            expect(actualFragment.fragmentType !== FragmentType.PossibleDate).toBeTruthy();
+        });
     });
 });
