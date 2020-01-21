fbd056cabcf283d1d204a5bc2ff00d768e94369b
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-21 22:18:26 +0630

    Update tests

--- test/specs/myanmar-text-fragmenter-Alphabet.spec.ts
+++ test/specs/myanmar-text-fragmenter-Alphabet.spec.ts
@@ -143,51 +143,6 @@ describe('MyanmarTextFragmenter#getNextFragment#alphabet', () => {
         });
     });
 
-    // // ၊ -> ၊
-    // it(String.raw`'\u104A' -> '\u104A'`, () => {
-    //     const fragment = fragmenter.getNextFragment('\u104A') as TextFragment;
-
-    //     expect(fragment.matchedString).toBe('\u104A',
-    //         `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
-    //     expect(fragment.error).toBeFalsy();
-    // });
-
-    // // ။ -> ။
-    // it(String.raw`'\u104B' -> '\u104B'`, () => {
-    //     const fragment = fragmenter.getNextFragment('\u104B') as TextFragment;
-
-    //     expect(fragment.matchedString).toBe('\u104B',
-    //         `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
-    //     expect(fragment.error).toBeFalsy();
-    // });
-
-    // // ၌ -> ၌
-    // it(String.raw`'\u104C' -> '\u104C'`, () => {
-    //     const fragment = fragmenter.getNextFragment('\u104C') as TextFragment;
-
-    //     expect(fragment.matchedString).toBe('\u104C',
-    //         `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
-    //     expect(fragment.error).toBeFalsy();
-    // });
-
-    // // ၍ -> ၍
-    // it(String.raw`'\u104D' -> '\u104D'`, () => {
-    //     const fragment = fragmenter.getNextFragment('\u104D') as TextFragment;
-
-    //     expect(fragment.matchedString).toBe('\u104D',
-    //         `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
-    //     expect(fragment.error).toBeFalsy();
-    // });
-
-    // // ၏ -> ၏
-    // it(String.raw`'\u104F' -> '\u104F'`, () => {
-    //     const fragment = fragmenter.getNextFragment('\u104F') as TextFragment;
-
-    //     expect(fragment.matchedString).toBe('\u104F',
-    //         `\n\nActual fragmentedString: ${formatCodePoints(fragment.matchedString)}`);
-    //     expect(fragment.error).toBeFalsy();
-    // });
-
     // // ဤက -> ဤ
     // it(String.raw`'\u1024\u1000' -> '\u1024'`, () => {
     //     const fragment = fragmenter.getNextFragment('\u1024\u1000') as TextFragment;
