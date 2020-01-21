0d38b761ccceacca44da01b66f7af973c4f0b1f7
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-21 22:30:42 +0630

    Update tests

---
+++ test/specs/myanmar-text-fragmenter-punctuation.spec.ts
@@ -0,0 +1,50 @@
+import { FragmentType } from '../../src/fragment-type';
+import { MyanmarTextFragmenter } from '../../src/myanmar-text-fragmenter';
+import { TextFragment } from '../../src/text-fragment';
+
+describe('MyanmarTextFragmenter#getNextFragment#punctuation', () => {
+    let fragmenter: MyanmarTextFragmenter;
+
+    beforeEach(() => {
+        fragmenter = new MyanmarTextFragmenter();
+    });
+
+    // ၌
+    it(String.raw`should return alphabet fragment when input '\u104C'`, () => {
+        const input = '\u104C';
+        const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+        const expactedFragment: TextFragment = {
+            matchedStr: input,
+            normalizedStr: input,
+            fragmentType: FragmentType.Punctuation
+        };
+
+        expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    // ၍
+    it(String.raw`should return alphabet fragment when input '\u104D'`, () => {
+        const input = '\u104D';
+        const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+        const expactedFragment: TextFragment = {
+            matchedStr: input,
+            normalizedStr: input,
+            fragmentType: FragmentType.Punctuation
+        };
+
+        expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    // ၏
+    it(String.raw`should return alphabet fragment when input '\u104F'`, () => {
+        const input = '\u104F';
+        const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+        const expactedFragment: TextFragment = {
+            matchedStr: input,
+            normalizedStr: input,
+            fragmentType: FragmentType.Punctuation
+        };
+
+        expect(actualFragment).toEqual(expactedFragment);
+    });
+});
