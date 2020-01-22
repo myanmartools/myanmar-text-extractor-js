8eca53e852bffcf8021bc5bdb91fb3446d6088af
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-22 21:37:00 +0630

    Update tests

--- test/specs/myanmar-text-fragmenter-time.spec.ts
+++ test/specs/myanmar-text-fragmenter-time.spec.ts
@@ -9,29 +9,46 @@ describe('MyanmarTextFragmenter#getNextFragment#time', () => {
         fragmenter = new MyanmarTextFragmenter();
     });
 
-    // hh:mm:ss
-    it(String.raw`should return time fragment when input '၂၃:၅၉:၅၉'`, () => {
-        const input = '၂၃:၅၉:၅၉';
-        const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
-        const expactedFragment: TextFragment = {
-            matchedStr: input,
-            normalizedStr: input,
-            fragmentType: FragmentType.PossibleTime
-        };
+    describe('hour-minute-second', () => {
+        // hh:mm:ss
+        it(String.raw`should return time fragment when input '၂၃:၅၉:၅၉'`, () => {
+            const input = '၂၃:၅၉:၅၉';
+            const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+            const expactedFragment: TextFragment = {
+                matchedStr: input,
+                normalizedStr: input,
+                fragmentType: FragmentType.PossibleTime
+            };
 
-        expect(actualFragment).toEqual(expactedFragment);
+            expect(actualFragment).toEqual(expactedFragment);
+        });
     });
 
-    // hh:mm
-    it(String.raw`should return time fragment when input '၂၃:၅၉'`, () => {
-        const input = '၂၃:၅၉';
-        const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
-        const expactedFragment: TextFragment = {
-            matchedStr: input,
-            normalizedStr: input,
-            fragmentType: FragmentType.PossibleTime
-        };
+    describe('hour-minute', () => {
+        // hh:mm
+        it(String.raw`should return time fragment when input '၂၃:၅၉'`, () => {
+            const input = '၂၃:၅၉';
+            const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+            const expactedFragment: TextFragment = {
+                matchedStr: input,
+                normalizedStr: input,
+                fragmentType: FragmentType.PossibleTime
+            };
 
-        expect(actualFragment).toEqual(expactedFragment);
+            expect(actualFragment).toEqual(expactedFragment);
+        });
+
+        // hh:mm
+        it(String.raw`should return time fragment when input '၀၁:၀၅'`, () => {
+            const input = '၀၁:၀၅';
+            const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+            const expactedFragment: TextFragment = {
+                matchedStr: input,
+                normalizedStr: input,
+                fragmentType: FragmentType.PossibleTime
+            };
+
+            expect(actualFragment).toEqual(expactedFragment);
+        });
     });
 });
