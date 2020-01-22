c7fd9677ee11d89186f0618ffad51c9a93be80e1
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-22 21:29:05 +0630

    Update tests

---
+++ test/specs/myanmar-text-fragmenter-time.spec.ts
@@ -0,0 +1,24 @@
+import { FragmentType } from '../../src/fragment-type';
+import { MyanmarTextFragmenter } from '../../src/myanmar-text-fragmenter';
+import { TextFragment } from '../../src/text-fragment';
+
+describe('MyanmarTextFragmenter#getNextFragment#time', () => {
+    let fragmenter: MyanmarTextFragmenter;
+
+    beforeEach(() => {
+        fragmenter = new MyanmarTextFragmenter();
+    });
+
+    // hh:mm:ss
+    it(String.raw`should return time fragment when input '၂၃:၅၉:၅၉'`, () => {
+        const input = '၂၃:၅၉:၅၉';
+        const actualFragment = fragmenter.getNextFragment(input) as TextFragment;
+        const expactedFragment: TextFragment = {
+            matchedStr: input,
+            normalizedStr: input,
+            fragmentType: FragmentType.PossibleTime
+        };
+
+        expect(actualFragment).toEqual(expactedFragment);
+    });
+});
