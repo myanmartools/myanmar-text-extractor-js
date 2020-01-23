2c1c2fcba704f43787ea38db5cd3aca45f8df9f4
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-23 07:32:02 +0630

    Update InvalidReason model

--- src/invalid-reason.ts
+++ src/invalid-reason.ts
@@ -2,6 +2,7 @@ export interface InvalidReason {
     invalidUnicodeForm?: boolean;
     invalidDiacriticsStart?: boolean;
     invalidSpaceIncluded?: boolean;
+    invalidCharInsteadOfColon?: boolean;
     invalidU101DInsteadOfU1040?: boolean;
     invalidU104EInsteadOfU1044?: boolean;
 }
