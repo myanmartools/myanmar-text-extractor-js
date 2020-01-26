067c0fcf55c679eb7daf4f5273399b8d573bc73e
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-26 20:53:11 +0630

    Update test files

--- test/specs/myanmar-text-extractor-punctuation.spec.ts
+++
@@ -1,4 +0,0 @@
-import { FragmentType } from '../../src/fragment-type';
-import { MyanmarTextExtractor } from '../../src/myanmar-text-extractor';
-import { TextFragment } from '../../src/text-fragment';
-
