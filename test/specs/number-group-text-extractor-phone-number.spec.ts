3c7474fd7968cb105904365a29b5ad2fbbf0e881
Moe Myat Zaw <mmzliveid@gmail.com>
2020-01-28 09:31:19 +0630

    Update tests

--- test/specs/number-group-text-extractor-phone-number.spec.ts
+++ test/specs/number-group-text-extractor-phone-number.spec.ts
@@ -278,6 +278,43 @@ describe('NumberGroupTextExtractor#phone-number', () => {
             expect(actualFragment).toEqual(expactedFragment);
         });
 
+        it(String.raw`should return phone number fragment when input '၀၁/၁၂၃၄၅၆'`, () => {
+            const input = '၀၁/၁၂၃၄၅၆';
+            const actualFragment = extractor.extractNext(input) as TextFragment;
+            const expactedFragment: TextFragment = {
+                matchedStr: input,
+                normalizedStr: input,
+                possiblePhoneNumber: true
+            };
+
+            expect(actualFragment).toEqual(expactedFragment);
+        });
+
+        it(String.raw`should return phone number fragment when input '၀၁/၁၂၃-၄၅၆'`, () => {
+            const input = '၀၁/၁၂၃-၄၅၆';
+            const actualFragment = extractor.extractNext(input) as TextFragment;
+            const expactedFragment: TextFragment = {
+                matchedStr: input,
+                normalizedStr: input,
+                possiblePhoneNumber: true
+            };
+
+            expect(actualFragment).toEqual(expactedFragment);
+        });
+
+        it(String.raw`should return phone number fragment when input '၀၁ / ၁၂၃၄၅၆'`, () => {
+            const input = '၀၁ / ၁၂၃၄၅၆';
+            const actualFragment = extractor.extractNext(input) as TextFragment;
+            const expactedFragment: TextFragment = {
+                matchedStr: input,
+                normalizedStr: input,
+                possiblePhoneNumber: true,
+                spaceIncluded: true
+            };
+
+            expect(actualFragment).toEqual(expactedFragment);
+        });
+
         it(String.raw`should return phone number fragment when input '၁_၂၃၄_၅၆၇'`, () => {
             const input = '၁_၂၃၄_၅၆၇';
             const actualFragment = extractor.extractNext(input) as TextFragment;
