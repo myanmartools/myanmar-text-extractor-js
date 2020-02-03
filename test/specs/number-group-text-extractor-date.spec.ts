0ec45e9ad80235116a2cdba82d7fc90ed468dc0d
Moe Myat Zaw <mmzliveid@gmail.com>
2020-02-03 23:14:18 +0630

    Update tests

--- test/specs/number-group-text-extractor-date.spec.ts
+++ test/specs/number-group-text-extractor-date.spec.ts
@@ -623,6 +623,13 @@ describe('NumberGroupTextExtractor#date', () => {
         });
 
         // Invalid ends (isValidRightStrForDate)
+        it(String.raw`should NOT return fragment when input '၀၁-၀၁-၂၀၂၀%က'`, () => {
+            const input = '၀၁-၀၁-၂၀၂၀%က';
+            const fragment = extractor.extractNext(input, input.codePointAt(0) as number);
+            expect(fragment == null || !fragment.possibleDate).toBeTruthy();
+        });
+
+        // Invalid ends (isValidRightStrForDate)
         it(String.raw`should NOT return fragment when input '၀၁-၀၁-၂၀၂၀_၁'`, () => {
             const input = '၀၁-၀၁-၂၀၂၀_၁';
             const fragment = extractor.extractNext(input, input.codePointAt(0) as number);
