a3c90f22f668080bec83e7d4b1502413a5a41d59
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-14 23:18:14 +0630

    Update extractPunctuationSymbol tests

--- test/specs/punctuation-symbol-text-extractor.spec.ts
+++ test/specs/punctuation-symbol-text-extractor.spec.ts
@@ -21,4 +21,37 @@ describe('extractPunctuationSymbol', () => {
 
         void expect(actualFragment).toEqual(expactedFragment);
     });
+
+    it(String.raw`should return 'PunctuationSymbolTextFragment' when input '။'`, () => {
+        const input = '။';
+
+        const actualFragment = extractPunctuationSymbol({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        const expactedFragment: PunctuationSymbolTextFragment = {
+            category: 'punctuation-symbol',
+            matchedStr: input,
+            uniProbability: 0.5,
+            zgProbability: 0.5
+        };
+
+        void expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    it(String.raw`should return 'null' when input 'က'`, () => {
+        const input = 'က';
+
+        const actualFragment = extractPunctuationSymbol({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        void expect(actualFragment).toBeNull();
+    });
 });
