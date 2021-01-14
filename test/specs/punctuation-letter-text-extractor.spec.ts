d0a70ec268d8b3a805bdd3601e779cb300158853
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-15 00:08:16 +0630

    Add punctuation-letter-text-extractor.spec.ts

---
+++ test/specs/punctuation-letter-text-extractor.spec.ts
@@ -0,0 +1,77 @@
+import { extractPunctuationLetter } from '../../src/punctuation-letter-text-extractor';
+import { PunctuationLetterTextFragment } from '../../src/punctuation-letter-text-fragment';
+
+describe('extractPunctuationLetter', () => {
+    it(String.raw`should return 'PunctuationLetterTextFragment' when input '၌'`, () => {
+        const input = '၌';
+
+        const actualFragment = extractPunctuationLetter({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        const expactedFragment: PunctuationLetterTextFragment = {
+            category: 'punctuation-letter',
+            matchedStr: input,
+            uniProbability: 0.5,
+            zgProbability: 0.5
+        };
+
+        void expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    it(String.raw`should return 'PunctuationSymbolTextFragment' when input '၍'`, () => {
+        const input = '၍';
+
+        const actualFragment = extractPunctuationLetter({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        const expactedFragment: PunctuationLetterTextFragment = {
+            category: 'punctuation-letter',
+            matchedStr: input,
+            uniProbability: 0.5,
+            zgProbability: 0.5
+        };
+
+        void expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    it(String.raw`should return 'PunctuationSymbolTextFragment' when input '၏'`, () => {
+        const input = '၏';
+
+        const actualFragment = extractPunctuationLetter({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        const expactedFragment: PunctuationLetterTextFragment = {
+            category: 'punctuation-letter',
+            matchedStr: input,
+            uniProbability: 0.5,
+            zgProbability: 0.5
+        };
+
+        void expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    it(String.raw`should return 'null' when input 'က'`, () => {
+        const input = 'က';
+
+        const actualFragment = extractPunctuationLetter({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        void expect(actualFragment).toBeNull();
+    });
+});
