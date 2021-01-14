bba2cc29082e351c678b8069e643f67eae3c9b97
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-14 11:38:46 +0630

    Add public_api

---
+++ public_api.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright copyright-name. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * @module
+ * @description
+ * Entry point for all public APIs of this package.
+ */
+// export * from './src/index';
