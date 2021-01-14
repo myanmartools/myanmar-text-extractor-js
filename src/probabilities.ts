1a3e553738aff8abe367f47fdf6845ed4cbd85ff
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-14 22:46:20 +0630

    Add probabilities.ts

---
+++ src/probabilities.ts
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export const p100 = 1;
+export const p50 = 0.5;
+export const p48 = 0.48;
+export const p45 = 0.45;
+export const p30 = 0.3;
+export const p0 = 0;
