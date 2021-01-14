88300a0edbad497ba4ed928c68fad27a5db7ba63
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-14 11:52:40 +0630

    Update project

--- src/text-extractor.ts
+++
@@ -1,5 +0,0 @@
-import { TextFragment } from './text-fragment';
-
-export interface TextExtractor {
-    extractNext(input: string, firstCp?: number, prevFragments?: TextFragment[]): TextFragment | null;
-}
