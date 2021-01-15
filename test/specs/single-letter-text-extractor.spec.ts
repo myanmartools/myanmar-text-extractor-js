367d718fa7b6d3d455bbb1624ddaa6c8079a2e18
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-15 17:18:41 +0630

    Update extractSingleLetter specs

--- test/specs/single-letter-text-extractor.spec.ts
+++ test/specs/single-letter-text-extractor.spec.ts
@@ -21,4 +21,84 @@ describe('extractSingleLetter', () => {
 
         void expect(actualFragment).toEqual(expactedFragment);
     });
+
+    it(String.raw`should return 'SingleLetterTextFragment' when input 'ဪ'`, () => {
+        const input = 'ဪ';
+
+        const actualFragment = extractSingleLetter({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        const expactedFragment: SingleLetterTextFragment = {
+            category: 'single-letter',
+            matchedStr: input[0],
+            uniProbability: 0.5,
+            zgProbability: 0.5
+        };
+
+        void expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    it(String.raw`should return 'SingleLetterTextFragment' when input 'က'`, () => {
+        const input = 'က';
+
+        const actualFragment = extractSingleLetter({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        const expactedFragment: SingleLetterTextFragment = {
+            category: 'single-letter',
+            matchedStr: input[0],
+            uniProbability: 0.5,
+            zgProbability: 0.5
+        };
+
+        void expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    it(String.raw`should return 'SingleLetterTextFragment' when input 'အ'`, () => {
+        const input = 'အ';
+
+        const actualFragment = extractSingleLetter({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        const expactedFragment: SingleLetterTextFragment = {
+            category: 'single-letter',
+            matchedStr: input[0],
+            uniProbability: 0.5,
+            zgProbability: 0.5
+        };
+
+        void expect(actualFragment).toEqual(expactedFragment);
+    });
+
+    it(String.raw`should return 'SingleLetterTextFragment' when input 'ဣ'`, () => {
+        const input = 'ဣ';
+
+        const actualFragment = extractSingleLetter({
+            totalTrimedInputLength: input.trim().length,
+            curStr: input,
+            firstCp: input.codePointAt(0) as number,
+            trimedCurStrLength: input.trim().length
+        });
+
+        const expactedFragment: SingleLetterTextFragment = {
+            category: 'single-letter',
+            matchedStr: input[0],
+            uniProbability: 0.5,
+            zgProbability: 0.5
+        };
+
+        void expect(actualFragment).toEqual(expactedFragment);
+    });
 });
