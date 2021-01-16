6a99ac6c06d1c696a5d29d8546777b0e3e2625b4
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-16 10:24:34 +0630

    Add single-dotted-letter-text-fragment.ts

---
+++ src/single-dotted-letter-text-fragment.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { TextFragment } from './text-fragment';
+
+export interface SingleDottedLetterTextFragment extends TextFragment {
+    category: 'single-dotted-letter';
+}
