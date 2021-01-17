522fb239c8e3c8473053464efa1401a831a6109c
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-17 19:09:45 +0630

    Add kinsi-text-fragment.ts

---
+++ src/kinsi-text-fragment.ts
@@ -0,0 +1,16 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { TextFragment } from './text-fragment';
+
+export interface KinsiTextFragment extends TextFragment {
+    category: 'kinsi';
+    kinsiFragmentOnly?: boolean;
+    leftLetterRequired?: boolean;
+    rightLetterRequired?: boolean;
+}
