7223cb90f957e943ed5a874c00d18a01a6fe4ec0
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-17 19:08:53 +0630

    Update probabilities.ts

--- src/probabilities.ts
+++ src/probabilities.ts
@@ -8,11 +8,19 @@
 
 export const p100 = 1;
 export const p90 = 0.9;
+export const p80 = 0.8;
+export const p70 = 0.7;
 export const p60 = 0.6;
 export const p55 = 0.55;
+export const p54 = 0.54;
+export const p53 = 0.53;
+export const p52 = 0.52;
+export const p51 = 0.51;
 export const p50 = 0.5;
 export const p48 = 0.48;
 export const p45 = 0.45;
 export const p40 = 0.4;
 export const p30 = 0.3;
+export const p20 = 0.2;
+export const p10 = 0.1;
 export const p0 = 0;
