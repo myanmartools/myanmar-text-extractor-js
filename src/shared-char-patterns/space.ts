b5f7ec29f388e7be3bad98dca4636a2f412fbd63
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-17 21:57:05 +0630

    Add shared-char-patterns

---
+++ src/shared-char-patterns/space.ts
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export const visibleSpace = ' \u00A0\u1680\u2000-\u2009\u202F\u205F\u3000';
+export const invisibleSpace = '\u00AD\u180E\u200A\u200B\u2060\uFEFF';
