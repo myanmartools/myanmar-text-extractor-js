4d422b96f95a087e6aa9296e3cad39fcd11302f3
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-20 10:36:17 +0630

    Rename to letter-text-extractor.ts

---
+++ src/letter-text-extractor.ts
@@ -0,0 +1,71 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { ExtractInfo } from './extract-info';
+import { LetterTextFragment } from './letter-text-fragment';
+
+import { p0, p100, p30, p45, p48, p50, p90 } from './probabilities';
+
+/**
+ * Extract single letter.
+ * ဤ / ဪ / [က-အ] / ဣ / ဥ / ဦ / ဧ / ဩ (တစ်လုံးတည်းဖြစ်ခဲ့လျှင်)
+ * uni: ဿ (တစ်လုံးတည်းဖြစ်ခဲ့လျှင်)
+ * uni: ၎ (သို့) zg: ၎င်း (တစ်လုံးတည်းဖြစ်ခဲ့လျှင်)
+ * @param extractInfo ExtractInfo object.
+ * @returns Returns the LetterTextFragment object.
+ */
+export function extractSingleLetter(extractInfo: Readonly<ExtractInfo>): LetterTextFragment | null {
+    const firstCp = extractInfo.firstCp;
+    const trimedCurStrLength = extractInfo.trimedCurStrLength;
+
+    let matchedStr = '';
+    let uniProbability = 0;
+    let zgProbability = 0;
+
+    if (
+        firstCp === 0x1024 ||
+        firstCp === 0x102a ||
+        (trimedCurStrLength === 1 &&
+            ((firstCp >= 0x1000 && firstCp <= 0x1021) ||
+                firstCp === 0x1023 ||
+                (firstCp >= 0x1025 && firstCp <= 0x1027) ||
+                firstCp === 0x1029))
+    ) {
+        matchedStr = extractInfo.curStr[0];
+        uniProbability = zgProbability = p50;
+    } else if (trimedCurStrLength === 1 && firstCp === 0x103f) {
+        matchedStr = extractInfo.curStr[0];
+        uniProbability = p100;
+        zgProbability = p0;
+    } else if (trimedCurStrLength === 1 && firstCp === 0x104e) {
+        matchedStr = extractInfo.curStr[0];
+        if (extractInfo.lastKnownWritingStyle === 'zg') {
+            uniProbability = p30;
+            zgProbability = p90;
+        } else {
+            uniProbability =
+                extractInfo.lastKnownWritingStyle === 'uni' &&
+                extractInfo.lastKnownWritingStyleProbability &&
+                extractInfo.lastKnownWritingStyleProbability > p50
+                    ? p48
+                    : p45;
+            zgProbability = p50;
+        }
+    }
+
+    if (!matchedStr.length) {
+        return null;
+    }
+
+    return {
+        category: 'letter',
+        matchedStr,
+        uniProbability,
+        zgProbability
+    };
+}
