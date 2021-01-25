87276a2a7735a3fbc455b1b42337c9a49131faa2
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-25 20:44:21 +0630

    Add char-patterns.ts

---
+++ src/char-patterns.ts
@@ -0,0 +1,15 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+export const visibleSpace = ' \u00A0\u1680\u2000-\u2009\u202F\u205F\u3000';
+export const invisibleSpace = '\u00AD\u180E\u200A\u200B\u2060\uFEFF';
+export const optDiacBf31 = '\u103A?\u103B?\u103C?(\u103D\u103E|[\u103D\u103E])?\u103A?';
+export const optDiacAf31 =
+    '[\u102D\u102E]?[\u102F\u1030]?[\u102B\u102C]?\u103A?[\u1032\u1036]?[\u102B\u102C]?\u103A?\u1037?[\u102B\u102C]?\u103A?\u1038?';
+export const cNonDiacAndPunc = '\u1000-\u102A\u103F\u1040-\u1049\u104E';
+export const cUdPs = '\u1000-\u101C\u101E-\u1022\u1027\u103F';
