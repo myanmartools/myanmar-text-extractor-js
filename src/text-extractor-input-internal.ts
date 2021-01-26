eaa11a5fb81e55d8f010899d0024762bac6c7172
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-26 12:31:56 +0630

    Add text-extractor-input-internal.ts

---
+++ src/text-extractor-input-internal.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { TextExtractorInput } from './text-extractor-input';
+
+export interface TextExtractorInputInternal extends TextExtractorInput {
+    firstCp: number;
+}
