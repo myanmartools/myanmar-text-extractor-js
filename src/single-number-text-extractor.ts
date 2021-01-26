08082f347aa66f0ca48a8d9ff1542aa5339f4d5d
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-26 12:38:14 +0630

    Update single-number-text-extractor.ts

---
+++ src/single-number-text-extractor.ts
@@ -0,0 +1,29 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { TextExtractorInputInternal } from './text-extractor-input-internal';
+import { TextFragment } from './text-fragment';
+
+export function extractSingleNumber(input: Readonly<TextExtractorInputInternal>): TextFragment | null {
+    const firstCp = input.firstCp;
+    const curStr = input.curStr;
+
+    if (firstCp >= 0x1040 && firstCp <= 0x1049 && curStr.trim().length === 1) {
+        const matchedStr = curStr[0];
+
+        return {
+            category: 'number',
+            matchedStr,
+            decimalStr: matchedStr,
+            uniProbability: 0.5,
+            zgProbability: 0.5
+        };
+    }
+
+    return null;
+}
