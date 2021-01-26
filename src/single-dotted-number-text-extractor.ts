54fe33b045a255c5bdc93614d501d4f7a3c386d3
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-26 13:59:31 +0630

    Update single-dotted-number-text-extractor.ts

--- src/single-dotted-number-text-extractor.ts
+++ src/single-dotted-number-text-extractor.ts
@@ -12,9 +12,9 @@ import { TextFragment } from './text-fragment';
 export function extractSingleDottedNumber(input: Readonly<TextExtractorInputInternal>): TextFragment | null {
     if (
         input.curStrRightTrimedLength === 2 &&
+        input.curStr[1] === '\uFE00' &&
         input.firstCp >= 0x1040 &&
-        input.firstCp <= 0x1049 &&
-        input.curStr[1] !== '\uFE00'
+        input.firstCp <= 0x1049
     ) {
         const matchedStr = input.curStr.substring(0, 2);
 
