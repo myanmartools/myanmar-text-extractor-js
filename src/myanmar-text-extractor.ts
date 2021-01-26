6257205f2c7d87fe08fe7364497f096a54d0d185
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-26 11:30:06 +0630

    Add myanmar-text-extractor.ts

---
+++ src/myanmar-text-extractor.ts
@@ -0,0 +1,22 @@
+/**
+ * @license
+ * Copyright DagonMetric. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found under the LICENSE file in the root directory of this source tree.
+ */
+
+import { TextExtractorInput } from './text-extractor-input';
+import { TextFragment } from './text-fragment';
+
+export class MyanmarTextExtractor {
+    extractNext(input: Readonly<TextExtractorInput>): TextFragment | null {
+        const curStr = input.curStr;
+        const firstCp = curStr.codePointAt(0);
+        if (firstCp == null) {
+            return null;
+        }
+
+        return null;
+    }
+}
