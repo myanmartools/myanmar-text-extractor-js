dc7e646124e7259d74c2bd79aa330f09deb13999
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-26 12:59:56 +0630

    Update single-number-text-extractor.ts

--- src/single-number-text-extractor.ts
+++ src/single-number-text-extractor.ts
@@ -12,8 +12,9 @@ import { TextFragment } from './text-fragment';
 export function extractSingleNumber(input: Readonly<TextExtractorInputInternal>): TextFragment | null {
     const firstCp = input.firstCp;
     const curStr = input.curStr;
+    const trimedCurStrLength = curStr.trimEnd().length;
 
-    if (firstCp >= 0x1040 && firstCp <= 0x1049 && curStr.trim().length === 1) {
+    if (firstCp >= 0x1040 && firstCp <= 0x1049 && trimedCurStrLength === 1) {
         const matchedStr = curStr[0];
 
         return {
