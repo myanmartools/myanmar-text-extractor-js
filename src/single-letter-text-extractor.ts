099974aea08f26933c6bdf28ac590dd93404cd2a
Moe Myat Zaw <mmzliveid@gmail.com>
2021-01-26 13:14:17 +0630

    Update single-letter-text-extractor.ts

--- src/single-letter-text-extractor.ts
+++ src/single-letter-text-extractor.ts
@@ -10,31 +10,27 @@ import { TextExtractorInputInternal } from './text-extractor-input-internal';
 import { TextFragment } from './text-fragment';
 
 export function extractSingleLetter(input: Readonly<TextExtractorInputInternal>): TextFragment | null {
-    const firstCp = input.firstCp;
-    const curStr = input.curStr;
-    const trimedCurStrLength = curStr.trimEnd().length;
-
     let matchedStr = '';
     let uniProbability = 0;
     let zgProbability = 0;
 
     if (
-        firstCp === 0x1024 ||
-        firstCp === 0x102a ||
-        (trimedCurStrLength === 1 &&
-            ((firstCp >= 0x1000 && firstCp <= 0x1021) ||
-                firstCp === 0x1023 ||
-                (firstCp >= 0x1025 && firstCp <= 0x1027) ||
-                firstCp === 0x1029))
+        input.firstCp === 0x1024 ||
+        input.firstCp === 0x102a ||
+        (input.curStrRightTrimedLength === 1 &&
+            ((input.firstCp >= 0x1000 && input.firstCp <= 0x1021) ||
+                input.firstCp === 0x1023 ||
+                (input.firstCp >= 0x1025 && input.firstCp <= 0x1027) ||
+                input.firstCp === 0x1029))
     ) {
-        matchedStr = curStr[0];
+        matchedStr = input.curStr[0];
         uniProbability = zgProbability = 0.5;
-    } else if (trimedCurStrLength === 1 && firstCp === 0x103f) {
-        matchedStr = curStr[0];
+    } else if (input.curStrRightTrimedLength === 1 && input.firstCp === 0x103f) {
+        matchedStr = input.curStr[0];
         uniProbability = 1;
         zgProbability = 0;
-    } else if (trimedCurStrLength === 1 && firstCp === 0x104e) {
-        matchedStr = curStr[0];
+    } else if (input.curStrRightTrimedLength === 1 && input.firstCp === 0x104e) {
+        matchedStr = input.curStr[0];
         if (input.prevMMWritingStyle === 'zg') {
             uniProbability = 0.3;
             zgProbability = 0.9;
